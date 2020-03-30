c8b44a44621476190aaabd710477e8968fe4070a
meselu16 <62573360+meselu16@users.noreply.github.com>
2020-03-30 09:50:01 -0500

    Add files via upload

---
+++ email/loginjs.js
@@ -0,0 +1,11 @@
+
+function validate(){
+
+  var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    console.log(document.getElementById("username").value);
+    console.log(document.getElementById("password").value);
+
+    alert(username ,password);
+ 
+}
