aa6ccb987b02fdae3a3bbff78987a4ca76a5fd9e
meselu16 <62573360+meselu16@users.noreply.github.com>
2020-03-31 06:33:19 -0500

    Update clock.js

--- clock/clock.js
+++ clock/clock.js
@@ -6,8 +6,14 @@ mytime=setTimeout('display_ct()',refresh)
 
 function display_ct() {
 let x = new Date(); 
+let hr=x.getHours();
+let mins=x.getMinutes();
+let sec=x.getSeconds();
+if(hr <10 ){hr='0'+hr;}
+if(mins <10 ) {mins='0' + mins; }
+if(sec<10){second='0' + sec;}
 let x1=x.getFullYear() +"/" +(parseInt(x.getMonth(),10) + 1)+ "/" + x.getDate() ; 
-x1 = x1 + " - " +  x.getHours()+ ":" +  x.getMinutes() + ":" +  x.getSeconds();
+x1 = x1 + " - " +  hr+ ":" +  mins + ":" +  sec;
 document.getElementById('ct').innerHTML = x1;
 display_c();
 }
